09adb4d8214e41a0842a9c2428a3dc58364f7d55
Hakan-Yasar <supercoder@SuperCoders-Air.fritz.box>
2024-03-08 18:11:14 +0100

    add onboarding

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,7 +1,21 @@
-import './Onboarding.css'
+import "./Onboarding.css";
 
 const Onboarding = () => {
-  return <h1>Onboarding</h1>
-}
+  return (
+    <section className="onboarding">
+      <div className="onboarding__pics">
+        <img src="public/images/splash-line2.svg" alt="" />
+        <img src="public/images/boarding-logo.png" alt="Onboarding-pic" />
+        <img src="public/images/splash-line3.svg" alt="" />
+      </div>
 
-export default Onboarding
+      <div className="onboarding__allRecipe">
+        <h1>All recipe you needed</h1>
+        <p>5000+ healthy recipes made by people for your healthy life</p>
+        <a href="">Get Started</a>
+      </div>
+    </section>
+  );
+};
+
+export default Onboarding;
