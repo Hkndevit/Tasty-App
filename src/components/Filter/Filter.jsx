7be3e6b3a9da3ce2d08170791d00f0e7cbf0d8d7
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-08 22:03:32 +0100

    Add Filter Compontent to Search Area Page and Render Content

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,60 @@
 import './Filter'
+import { useEffect, useState } from 'react'
 
-const Filter = () => {
-  return <h1>Filter</h1>
+const Filter = ({ data }) => {
+  const [area, setArea] = useState('American')
+  const [meal, setMeal] = useState([])
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`).then(
+      (res) => res.json().then((data) => setMeal(data))
+    )
+  }, [area])
+
+  return (
+    <>
+      {/* Render all Areas */}
+      <section className="filter">
+        <article className="filter__data-container">
+          {data.meals ? (
+            data.meals.map((item, index) => {
+              return (
+                <div
+                  onClick={(e) => setArea(e.target.textContent)}
+                  className="filter__data"
+                  key={index}>
+                  {item.strArea}
+                </div>
+              )
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </article>
+      </section>
+      {/* Render Meals per Area */}
+      <section className="grid">
+        {meal.meals ? (
+          meal.meals.map((item) => {
+            return (
+              <div
+                className="grid__item"
+                key={item.idMeal}>
+                <img
+                  className="grid__item__image"
+                  src={item.strMealThumb}
+                  alt={item.strMeal}
+                />
+                <p className="grid__item__text">{item.strMeal}</p>
+              </div>
+            )
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </>
+  )
 }
 
 export default Filter
--- src/pages/SearchArea/SearchArea.jsx
+++ src/pages/SearchArea/SearchArea.jsx
@@ -1,10 +1,27 @@
-import Navigation from '../../components/Navigation/Navigation'
 import './SearchArea.css'
+import Filter from '../../components/Filter/Filter'
+import Navigation from '../../components/Navigation/Navigation'
+import { useEffect, useState } from 'react'
 
 const SearchArea = () => {
+  const [area, setArea] = useState([])
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list').then(
+      (res) =>
+        res
+          .json()
+          .then((data) => setArea(data))
+          .catch((err) => console.error('No data received', err))
+    )
+  }, [])
+
   return (
     <main>
-      <h1>SearchArea</h1>
+      <div className="search-area-wrapper">
+        <h1>SearchArea</h1>
+        <Filter data={area} />
+      </div>
       <Navigation />
     </main>
   )
