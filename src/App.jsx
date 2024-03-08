2232e2e4522938ff9f27030e6c6b145828287237
Reazy96 <resul.sadriu@outlook.com>
2024-03-08 18:09:26 +0100

    add loadingscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../src/pages/Home/Home'
-import Onboarding from '../src/pages/Onboarding/Onboarding'
-import SearchArea from '../src/pages/SearchArea/SearchArea'
-import SearchCategory from '../src/pages/SearchCategory/SearchCategory'
-import SearchResults from '../src/pages/SearchResults/SearchResults'
-import Details from '../src/pages/Details/Details'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home/Home";
+import Onboarding from "../src/pages/Onboarding/Onboarding";
+import SearchArea from "../src/pages/SearchArea/SearchArea";
+import SearchCategory from "../src/pages/SearchCategory/SearchCategory";
+import SearchResults from "../src/pages/SearchResults/SearchResults";
+import Details from "../src/pages/Details/Details";
+import Loading from "./pages/Loading/Loading";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/onboarding"
-          element={<Onboarding />}
-        />
-        <Route
-          path="/search/area"
-          element={<SearchArea />}
-        />
-        <Route
-          path="/search/category"
-          element={<SearchCategory />}
-        />
-        <Route
-          path="/search/results"
-          element={<SearchResults />}
-        />
-        <Route
-          path="/details"
-          element={<Details />}
-        />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/search/area" element={<SearchArea />} />
+        <Route path="/search/category" element={<SearchCategory />} />
+        <Route path="/search/results" element={<SearchResults />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,7 +1,76 @@
-import './Loading.css'
+import "./Loading.css";
 
 const Loading = () => {
-  return <h1>Loading</h1>
-}
+  return (
+    <section className="loading">
+      <div className="loading__line">
+        <svg width="186" height="99" viewBox="0 0 186 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M114.922 -79.5327C72.9515 -7.46804 -2.16703 -34.018 1.29143 31.8583C4.45791 92.1728 105.837 129.688 141.181 61.7922C166.509 13.1347 202.339 -58.9208 174.436 -87.3613"
+            stroke="#3DA0A7"
+            stroke-linecap="round"
+          />
+        </svg>
+        <svg width="375" height="192" viewBox="0 0 375 192" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M502.637 -110.177C490.218 -106.724 463.389 -85.817 455.423 -29.8112C445.465 40.1962 423.691 183.806 334.473 151.38C245.255 118.955 193.497 60.3343 220.541 34.452C247.585 8.56976 305.262 64.5545 241.8 141.082C178.338 217.609 42.2009 207.63 -13.9634 104.603"
+            stroke="#3DA0A7"
+          />
+        </svg>
+      </div>
+      <div className="loading__logo">
+        <svg width="27" height="17" viewBox="0 0 27 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M6.30005 15.1985C3.76876 14.7739 1.83997 12.5724 1.83997 9.92045C1.83997 6.96457 4.23618 4.56836 7.19206 4.56836C7.55934 4.56836 7.91797 4.60535 8.26444 4.67582"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+          <path
+            d="M20.7527 15.1985C23.284 14.7739 25.2128 12.5724 25.2128 9.92045C25.2128 6.96457 22.8166 4.56836 19.8607 4.56836C19.4934 4.56836 19.1348 4.60535 18.7883 4.67582"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+          <path
+            d="M8.25272 7.69012C8.14265 7.26246 8.08411 6.81411 8.08411 6.35209C8.08411 3.39621 10.4803 1 13.4362 1C16.3921 1 18.7883 3.39621 18.7883 6.35209C18.7883 6.81411 18.7298 7.26246 18.6197 7.69012"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+          <path d="M13.4011 10.6719V13.4748" stroke="white" stroke-width="2" stroke-linecap="round" />
+          <path
+            d="M8.44441 10.9072C8.22034 11.4077 8.63112 13.3158 9.1166 13.6286"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+          <path
+            d="M18.7255 10.9072C18.9496 11.4077 18.5388 13.3158 18.0533 13.6286"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+        </svg>
+        <h1>Tasty</h1>
+      </div>
+      <div className="loading__line">
+        <svg width="375" height="283" viewBox="0 0 375 283" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M449.514 327.169C462.083 284.964 461.374 192.234 405.163 156.94C348.953 121.647 289.808 124.497 216.474 163.743C142.356 203.41 47.165 199.838 64.211 131.425C87.429 38.2413 98.056 1.62099 32.4099 26.5024C-33.2362 51.3839 -6.44461 -9.73792 -80.5082 3.05578C-139.759 13.2907 -196.935 104.317 -251.649 2.22168"
+            stroke="#3DA0A7"
+          />
+        </svg>
+        <svg width="205" height="71" viewBox="0 0 205 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M65.2707 167.711C123.657 108.163 189.952 152.352 202.781 87.6442C214.527 28.3997 125.469 -32.8686 74.5309 24.2655C38.0263 65.2106 -14.4049 126.257 5.65634 160.68"
+            stroke="#3DA0A7"
+            stroke-linecap="round"
+          />
+        </svg>
+      </div>
+    </section>
+  );
+};
 
-export default Loading
+export default Loading;
