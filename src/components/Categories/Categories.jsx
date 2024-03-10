37c85cdf9ea0be0c5b23ab28e74b1fcff9781262
Reazy96 <resul.sadriu@outlook.com>
2024-03-10 20:39:24 +0100

    fetch and map done

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,32 @@
-import './Categories'
+import { useEffect, useState } from "react";
+import "./Categories.css";
 
 const Categories = () => {
-  return <h1>Categories</h1>
-}
+  const [catego, setCatego] = useState();
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((respo) => respo.json())
+      .then((allCatego) => setCatego(allCatego))
+      .catch((err) => console.error("Fehler in Catego-Fetch", err));
+  }, []);
 
-export default Categories
+  return (
+    <section>
+      <h2>Categories</h2>
+      <p>See All</p>
+
+      {catego ? (
+        catego.categories.map((singleCatego, index) => (
+          <div key={index}>
+            <img src={singleCatego.strCategoryThumb} alt="" />
+            <p>{singleCatego.strCategory}</p>
+          </div>
+        ))
+      ) : (
+        <p>loading..</p>
+      )}
+    </section>
+  );
+};
+
+export default Categories;
