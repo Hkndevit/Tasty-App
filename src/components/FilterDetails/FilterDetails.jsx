cc6eeded98cb0d13103f5dff338b489da89947e0
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-03-11 11:56:12 +0100

    add detailpage and detail filter

--- src/components/FilterDetails/FilterDetails.jsx
+++ src/components/FilterDetails/FilterDetails.jsx
@@ -1,7 +1,49 @@
-import './FilterDetails'
+import { useState } from "react";
+import "./FilterDetails.css";
 
-const FilterDetails = () => {
-  return <h1>FilterDetails</h1>
-}
+//! props: instructions, ingredient1-20
+const FilterDetails = (props) => {
+  //* state dafür, welcher Button getoggelt ist (instructions oder ingredients):
+  const [theme, setTheme] = useState(false);
 
-export default FilterDetails
+  //* Funktion für onclick:
+  const changeTheme = () => {
+    setTheme((item) => !item);
+  };
+  // console.log(theme);
+
+  return (
+    <section className="filter-details">
+      <div>
+        {/* //* Klick-Funktionen, um den state zu ändern: */}
+        <p onClick={changeTheme}>Ingredients</p>
+        <p onClick={changeTheme}>Instructions</p>
+      </div>
+
+      {/* //* je nach state hide- und show-classes hinzufügen: */}
+      <article className={`filter-details__ingredients ${theme ? "hide" : ""}`}>
+        <h3>Ingredients</h3>
+        {/* //* mit den Props die Inhalte aus dem Fetch von Details rendern: */}
+        <p>{props.ingredient8 != null ? props.ingredient8 : ""}</p>
+        <p>{props.ingredient2 != null ? props.ingredient2 : ""}</p>
+        <p>{props.ingredient3 != null ? props.ingredient3 : ""}</p>
+        <p>{props.ingredient4 != null ? props.ingredient4 : ""}</p>
+        <p>{props.ingredient5 != null ? props.ingredient5 : ""}</p>
+        <p>{props.ingredient6 != null ? props.ingredient6 : ""}</p>
+        <p>{props.ingredient7 != null ? props.ingredient7 : ""}</p>
+        <p>{props.ingredient8 != null ? props.ingredient8 : ""}</p>
+      </article>
+
+      {/* //* je nach state hide- und show-classes hinzufügen: */}
+      <article
+        className={`filter-details__instructions ${theme ? "show" : ""}`}
+      >
+        {/* //* mit den Props die Inhalte aus dem Fetch von Details rendern: */}
+        <h3>Instructions</h3>
+        <p>{props.instructions}</p>
+      </article>
+    </section>
+  );
+};
+
+export default FilterDetails;
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,13 +1,65 @@
-import Navigation from '../../components/Navigation/Navigation'
-import './Details.css'
+import { useEffect, useState } from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import FilterDetails from "../../components/FilterDetails/FilterDetails";
+import "./Details.css";
+import { useParams } from "react-router-dom";
+
+// Api-link nach ID: https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
 
 const Details = () => {
+  // state für gefetchte Daten:
+  const [detail, setDetail] = useState();
+
+  // Link auslesen, damit wir ihn im fetch-Link nutzen können:
+  const { id } = useParams();
+  console.log(id);
+
+  // Fetchen der Daten und speichern im state detail:
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((res) => res.json())
+      .then((data) => setDetail(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(detail);
+
   return (
     <main>
-      <h1>Details</h1>
+      <section className="details">
+        {detail ? (
+          <div>
+            <img
+              src={detail.meals[0].strMealThumb}
+              alt={detail.meals[0].strMeal}
+            />
+            <div className="details__textblock">
+              <h2>{detail.meals[0].strMeal}</h2>
+              <div className="details__textblock-desc">
+                <p>{detail.meals[0].strCategory}</p>
+                <p>{detail.meals[0].strArea}</p>
+              </div>
+              <FilterDetails
+                instructions={detail.meals[0].strInstructions}
+                ingredient1={detail.meals[0].strIngredient1}
+                ingredient2={detail.meals[0].strIngredient2}
+                ingredient3={detail.meals[0].strIngredient3}
+                ingredient4={detail.meals[0].strIngredient4}
+                ingredient5={detail.meals[0].strIngredient5}
+                ingredient6={detail.meals[0].strIngredient6}
+                ingredient7={detail.meals[0].strIngredient7}
+                ingredient8={detail.meals[0].strIngredient8}
+              />
+            </div>
+          </div>
+        ) : (
+          <p>Laden ...</p>
+        )}
+      </section>
+
       <Navigation />
     </main>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
