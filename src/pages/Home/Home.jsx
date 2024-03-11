9e12cd63b0078b724523794900e79cda1b0d8fca
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-11 12:00:56 +0100

    Add Filter to Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navigation from '../../components/Navigation/Navigation'
 import Categories from '../../components/Categories/Categories'
 import Search from '../../components/Search/Search'
 import Random from '../../components/Random/Random'
+import { FilterHome } from '../../components/FilterHome/FilterHome'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <h1>Home</h1>
       <Search />
       <Random />
+      <FilterHome />
       <Categories />
       <Navigation />
     </main>
