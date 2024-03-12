3211d0400f60e08392afd54bdc1203f6b8996387
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-03-12 13:57:02 +0100

    add styles

--- src/pages/SearchCategory/SearchCategory.jsx
+++ src/pages/SearchCategory/SearchCategory.jsx
@@ -1,27 +1,26 @@
-import './SearchCategory.css'
-import Navigation from '../../components/Navigation/Navigation'
-import SearchHeadline from '../../components/SearchHeadline/SearchHeadline'
-import { useEffect, useState, useContext } from 'react'
-import Filter from '../../components/Filter/Filter'
+import "./SearchCategory.css";
+import Navigation from "../../components/Navigation/Navigation";
+import SearchHeadline from "../../components/SearchHeadline/SearchHeadline";
+import { useEffect, useState, useContext } from "react";
+import Filter from "../../components/Filter/Filter";
 
 const SearchCategory = () => {
-  const [category, setCategory] = useState([])
+  const [category, setCategory] = useState([]);
 
   useEffect(() => {
-    fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
       .then((res) => res.json())
       .then((data) => setCategory(data))
-      .catch((err) => console.error('No data received', err))
-  }, [])
+      .catch((err) => console.error("No data received", err));
+  }, []);
 
   return (
     <main>
-      <h1>SearchCategory</h1>
       <SearchHeadline />
-      <Filter data={category} itemFilter='strCategory' />
+      <Filter data={category} itemFilter="strCategory" />
       <Navigation />
     </main>
-  )
-}
+  );
+};
 
-export default SearchCategory
+export default SearchCategory;
