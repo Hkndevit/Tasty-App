562961e4411e2e8d8ea6d2be10229c46aa465afd
Reazy96 <resul.sadriu@outlook.com>
2024-03-12 09:30:04 +0100

    search arrow with link

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import "./Random.css";
 
 const Random = () => {
-  const [random, setRandom] = useState();
-  useEffect(() => {
-    fetch("www.themealdb.com/api/json/v1/1/random.php")
-      .then((respo) => respo.json())
-      .then((data) => setCatego(data))
-      .catch((err) => console.error("Fehler im Code", err));
-  }, []);
+  // const [random, setRandom] = useState();
+  // useEffect(() => {
+  //   fetch("www.themealdb.com/api/json/v1/1/random.php")
+  //     .then((respo) => respo.json())
+  //     .then((data) => setCatego(data))
+  //     .catch((err) => console.error("Fehler im Code", err));
+  // }, []);
 
   return (
     <section className="random">
@@ -29,15 +29,8 @@ const Random = () => {
 
         <div className="random__wrapper">
           <div className="random__cardText">
-            <svg
-              className="circle"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="#c4c4c4"
-                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"
-              />{" "}
+            <svg className="circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="#c4c4c4" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />{" "}
             </svg>
             <p>Breakfast</p>
           </div>
--- src/components/SearchHeadline/SearchHeadline.jsx
+++ src/components/SearchHeadline/SearchHeadline.jsx
@@ -3,7 +3,9 @@ import "./SearchHeadline.css";
 const SearchHeadline = () => {
   return (
     <div className="search-headline">
-      <img src="/images/Arrow - Left.svg" alt="" />
+      <Link to="/">
+        <img src="/images/Arrow - Left.svg" alt="" />
+      </Link>
       <div className="search-headline__centered">
         <h1>Search</h1>
       </div>
