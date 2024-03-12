53fab7d93bd72ff87ec4594a3bb986740ca3abcf
Hakan-Yasar <supercoder@SuperCoders-Air.fritz.box>
2024-03-12 13:17:11 +0100

    random finished

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -3,39 +3,56 @@ import "./Random.css";
 import { useState, useEffect } from "react";
 
 const Random = () => {
-  // const [random, setRandom] = useState();
-  // useEffect(() => {
-  //   fetch("www.themealdb.com/api/json/v1/1/random.php")
-  //     .then((respo) => respo.json())
-  //     .then((data) => setCatego(data))
-  //     .catch((err) => console.error("Fehler im Code", err));
-  // }, []);
+  const [random, setRandom] = useState();
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((respo) => respo.json())
+      .then((data) => setRandom(data))
+      .catch((err) => console.error("Fehler im Code", err));
+  }, []);
+  console.log(random);
 
   return (
     <section className="random">
       <div>
-        <h1>Meal of the Day</h1>
+        <h3>Meal of the Day</h3>
       </div>
       <div className="random__card">
         <div className="random__foodPic">
           <img src="/images/Frame 490.png" alt="" />
         </div>
-        <div className="random__h1">
-          <Link className="randomLink" to="/details/52895">
-            {/* <h1>Full English Breakfast</h1> */}
-          </Link>
-        </div>
-
-        {/* Gerade Dabei  */}
-
-        <div className="random__wrapper">
-          <div className="random__cardText">
-            <svg className="circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path fill="#c4c4c4" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />{" "}
-            </svg>
-            {/* <p>Breakfast</p> */}
-          </div>
-          {/* <p>British</p> */}
+        <div>
+          {random ? (
+            random.meals.map((singleRandom, index) => (
+              <Link
+                className="randomLink"
+                to={`/details/${singleRandom.idMeal}`}
+                key={index}
+              >
+                <h4 className="random__h4">{singleRandom.strMeal}</h4>
+                <div className="random__wrapper">
+                  <div className="random__cardText">
+                    <svg
+                      className="circle"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    >
+                      <path
+                        fill="#c4c4c4"
+                        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"
+                      />
+                    </svg>
+                    <p>{singleRandom.strCategory}</p>
+                  </div>
+                  <div className="random__infoZeile">
+                    <p>{singleRandom.strArea}</p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>loading..</p>
+          )}
         </div>
       </div>
     </section>
