0bb0f1d96141da73ae58f5eaccfabf52d25b0402
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-13 08:59:16 +0100

    Add README + Screenshot

---
+++ README.md
@@ -0,0 +1,26 @@
+# Collaborative project: Tasty Recipe App
+
+## Purpose of the website
+
+- Create a recipe app
+- User can search recipes by country and category
+- User receives detailed page of the recipe with ingredients, quantities and instructions
+- User receives random recipe suggestions
+
+## Built with
+
+- Mobile Design
+- Grid and Flexbox Layout
+- React
+- React Router DOM
+
+## Links
+
+- [See live site here]()
+- [GitHub Repository](https://github.com/thomaserdmenger/Tasty-React)
+- [GitHub Author](https://github.com/thomaserdmenger)
+- [LinkedIn Author](https://www.linkedin.com/in/thomaserdmenger/)
+
+## Screenshot-Image
+
+![](./public/images/screenshot.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../src/pages/Home/Home";
-import Onboarding from "../src/pages/Onboarding/Onboarding";
-import SearchArea from "../src/pages/SearchArea/SearchArea";
-import SearchCategory from "../src/pages/SearchCategory/SearchCategory";
-import SearchResults from "../src/pages/SearchResults/SearchResults";
-import Details from "../src/pages/Details/Details";
-import Loading from "./pages/Loading/Loading";
-import { areaContext, categoryContext } from "./context/Context";
-import { useEffect, useState } from "react";
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "../src/pages/Home/Home"
+import Onboarding from "../src/pages/Onboarding/Onboarding"
+import SearchArea from "../src/pages/SearchArea/SearchArea"
+import SearchCategory from "../src/pages/SearchCategory/SearchCategory"
+import SearchResults from "../src/pages/SearchResults/SearchResults"
+import Details from "../src/pages/Details/Details"
+import Loading from "./pages/Loading/Loading"
+import { areaContext, categoryContext } from "./context/Context"
+import { useEffect, useState } from "react"
 
 function App() {
   // Text Content from Area Component in Home Page to FilterArea Component in Search Area Page
-  const [areaValue, setAreaValue] = useState("");
-  const [categoryValue, setCategoryValue] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [areaValue, setAreaValue] = useState("")
+  const [categoryValue, setCategoryValue] = useState("")
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => setLoading(true), 2000);
-  }, []);
+    setTimeout(() => setLoading(true), 2000)
+  }, [])
 
   return (
     <categoryContext.Provider value={{ categoryValue, setCategoryValue }}>
       <areaContext.Provider value={{ setAreaValue, areaValue }}>
         {loading ? (
-          <div className="wrapper">
+          <div className='wrapper'>
             <BrowserRouter>
               <Routes>
-                <Route path="/loading" element={<Loading />} />
-                <Route path="/" element={<Onboarding />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/search/area" element={<SearchArea />} />
-                <Route path="/search/category" element={<SearchCategory />} />
-                <Route path="/search/results" element={<SearchResults />} />
-                <Route path="/details/:id" element={<Details />} />
+                <Route path='/loading' element={<Loading />} />
+                <Route path='/' element={<Onboarding />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/search/area' element={<SearchArea />} />
+                <Route path='/search/category' element={<SearchCategory />} />
+                <Route path='/search/results' element={<SearchResults />} />
+                <Route path='/details/:id' element={<Details />} />
               </Routes>
             </BrowserRouter>
           </div>
@@ -42,7 +42,7 @@ function App() {
         )}
       </areaContext.Provider>
     </categoryContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
