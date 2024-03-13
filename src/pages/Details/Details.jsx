f67ccb38a189e8f45ebae55991773d4bc950b7bf
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-13 07:25:42 +0100

    Update Image Path

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from "react";
-import Navigation from "../../components/Navigation/Navigation";
-import FilterDetails from "../../components/FilterDetails/FilterDetails";
-import "./Details.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react"
+import Navigation from "../../components/Navigation/Navigation"
+import FilterDetails from "../../components/FilterDetails/FilterDetails"
+import "./Details.css"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
 
 const Details = () => {
   //* Variable für Navigate() für den Back-to-previous-site-Button:
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   //* state für gefetchte Daten:
-  const [detail, setDetail] = useState();
+  const [detail, setDetail] = useState()
 
   //* Variablen mit leerem Array, um die Ingredients und measures dort hinein zu pushen:
-  let ingredients = [];
-  let measures = [];
+  let ingredients = []
+  let measures = []
 
   //* dynamisches Link-Ende auslesen, damit wir ihn im fetch-Link nutzen können:
-  const { id } = useParams();
+  const { id } = useParams()
 
   //* Fetchen der Daten und Speichern im state namens detail:
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
       .then((res) => res.json())
       .then((data) => setDetail(data))
-      .catch((err) => console.log(err));
-  }, []);
+      .catch((err) => console.log(err))
+  }, [])
 
   //* Funktion zum Filtern der Daten nach Ingredients und null-Inhalte rausfiltern:
   const getIngredients = () => {
@@ -34,16 +34,16 @@ const Details = () => {
           typeof detail.meals[0][item] != "string" ||
           detail.meals[0][item] === ""
         ) {
-          return;
+          return
         } else {
-          ingredients.push(detail.meals[0][item]);
+          ingredients.push(detail.meals[0][item])
         }
       }
     }
-  };
+  }
 
   //* Funktionsaufruf für ingredients, aber nur, wenn in detail-state die Daten bereits enthalten sind:
-  detail ? getIngredients() : console.log("Loading ingredients");
+  detail ? getIngredients() : console.log("Loading ingredients")
 
   //* Funktion zum Filtern der Daten nach Measures und null-Inhalte rausfiltern:
   const getMeasures = () => {
@@ -53,38 +53,37 @@ const Details = () => {
           typeof detail.meals[0][item] != "string" ||
           detail.meals[0][item] === ""
         ) {
-          return;
+          return
         } else {
-          measures.push(detail.meals[0][item]);
+          measures.push(detail.meals[0][item])
         }
       }
     }
-  };
+  }
 
   //* Funktionsaufruf für mesasures, aber nur, wenn in detail-state die Daten bereits enthalten sind:
-  detail ? getMeasures() : console.log("Loading measures");
+  detail ? getMeasures() : console.log("Loading measures")
 
   return (
     <main>
-      <section className="details">
+      <section className='details'>
         {detail ? (
           <div>
             <svg
               onClick={() => navigate(-1)}
-              className="details--img__arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+              className='details--img__arrow'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 448 512'>
+              <path d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z' />
             </svg>
             <img
-              className="details--img__food"
+              className='details--img__food'
               src={detail.meals[0].strMealThumb}
               alt={detail.meals[0].strMeal}
             />
-            <div className="details__textblock">
+            <div className='details__textblock'>
               <h2>{detail.meals[0].strMeal}</h2>
-              <div className="details__textblock-desc">
+              <div className='details__textblock-desc'>
                 <p>{detail.meals[0].strCategory}</p>
                 <p>{detail.meals[0].strArea}</p>
               </div>
@@ -101,7 +100,7 @@ const Details = () => {
       </section>{" "}
       <Navigation />
     </main>
-  );
-};
+  )
+}
 
-export default Details;
+export default Details
--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom";
-import "./Onboarding.css";
+import { Link } from "react-router-dom"
+import "./Onboarding.css"
 
 const Onboarding = () => {
   return (
-    <section className="onboarding">
-      <div className="onboarding__pics">
-        <img src="public/images/splash-line2.svg" alt="" />
-        <img src="public/images/boarding-logo.png" alt="Onboarding-pic" />
-        <img src="public/images/splash-line3.svg" alt="" />
+    <section className='onboarding'>
+      <div className='onboarding__pics'>
+        <img src='/images/splash-line2.svg' alt='' />
+        <img src='/images/boarding-logo.png' alt='Onboarding-pic' />
+        <img src='/images/splash-line3.svg' alt='' />
       </div>
 
-      <div className="onboarding__allRecipe">
+      <div className='onboarding__allRecipe'>
         <h1>All the recipes you need</h1>
         <p>5000+ recipes made by people for your healthy life</p>
-        <Link to="/home">Get Started</Link>
+        <Link to='/home'>Get Started</Link>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Onboarding;
+export default Onboarding
